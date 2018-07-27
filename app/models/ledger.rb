53da881946efaf85bf0bade9b8d13c7c84ae94e7
SahilThakur7 <sahilrd07@gmail.com>
2018-07-27 18:34:50 +0530

    see updated_entitled array problem

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -31,7 +31,35 @@ class Ledger < ApplicationRecord
     end
 
 
+    def absolute_dealings_left_array(user)
+        usersleft = userpos(user)
+        dealings_left_array = []
 
+        entitled_hash.first(usersleft).each do |u|
+            dealings_left_array << calculate_dealing(u)
+        end
 
+        updated_entitled(user,dealings_left_array)
+
+        dealings_left_array
+    end
+
+    def userpos(user)
+        entitled_hash.index([user,self.entitled(user)])
+    end
+
+    def calculate_dealing(user)
+        (self.entitled(user)/ group.members.count - userpos(user)).abs
+    end
+
+    def sum_of_dealings_left(array)
+        array.inject do |running_total, num|
+            running_total + num
+        end
+    end
+
+    def updated_entitled(user,dealings_left_array)
+        self.entitled(user) + sum_of_dealings_left(dealings_left_array)
+    end
 
 end
